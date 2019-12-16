9c37bd3fe36b8e07dc22f2ef84ea2fb55603c8cf
Raphael Bizos <r.bizos@criteo.com>
2019-12-16 17:45:23 +0100

    added exemples for storage, cleaned the code

--- storage/metric_schema.go
+++ storage/metric_schema.go
@@ -1,32 +1,44 @@
 package storage
 
 import (
-	"gopkg.in/ini.v1"
+	"fmt"
 	"regexp"
-	)
+
+	"gopkg.in/ini.v1"
+)
 
 type StorageSchema struct {
-	ID         string
-	pattern    string
-	retentions string
-	patternRegex      *regexp.Regexp
+	ID           string
+	pattern      string
+	retentions   string
+	patternRegex *regexp.Regexp
 }
 
 func NewStorageSchemas(storageSchemasConf string) ([]StorageSchema, error) {
 	schemas := []StorageSchema{}
 	cfg, err := ini.Load(storageSchemasConf)
 	if err != nil {
-		return schemas, nil
+		return schemas, err
 	}
 	for _, section := range cfg.Sections()[1:] { // first element is empty default value
-		pattern := section.KeysHash()["PATTERN"]
+		pattern, ok := section.KeysHash()["PATTERN"]
+
+		if ok != true {
+			return schemas, fmt.Errorf("no pattern found in section %s in file %s", section.Name(), storageSchemasConf)
+		}
+		retentions, ok := section.KeysHash()["RETENTIONS"]
+
+		if ok != true {
+			return schemas, fmt.Errorf("no retention found in section %s in file %s", section.Name(), storageSchemasConf)
+		}
+
 		patternRegex, _ := regexp.Compile(pattern)
 		schemas = append(
 			schemas,
 			StorageSchema{
-				ID:         section.Name(),
-				pattern:    pattern,
-				retentions: section.KeysHash()["RETENTIONS"],
+				ID:           section.Name(),
+				pattern:      pattern,
+				retentions:   retentions,
 				patternRegex: patternRegex,
 			},
 		)
